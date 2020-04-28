7e2f86e8945a2bb9ad9238a4e3f51b2eb57a8c71
Trevor Momeyer <t.momeyer@setonhill.edu>
2020-04-28 11:47:03 -0400

    Initial Commit

---
+++ Final Project Bowling AppTests/Final_Project_Bowling_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Final_Project_Bowling_AppTests.swift
+//  Final Project Bowling AppTests
+//
+//  Created by Trevor Momeyer on 4/28/20.
+//  Copyright © 2020 Trevor Momeyer. All rights reserved.
+//
+
+import XCTest
+@testable import Final_Project_Bowling_App
+
+class Final_Project_Bowling_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
